a0d3b1e0a5763820734e1886a0bed3c05a9720b7
Sergey Master <ussoft@yahoo.com>
2014-07-13 19:41:47 -0400

    Exploratory Data Analysis Assignment 1

---
+++ plot3.R
@@ -0,0 +1,13 @@
+dat=read.csv("household_power_consumption.txt",sep=";")
+dat=dat[dat$Date %in% c("1/2/2007","2/2/2007"),]
+dat$Date=as.POSIXct(strptime(dat$Date, "%d/%m/%Y"))
+dat$Global_active_power=as.numeric(dat$Global_active_power)
+dat$Sub_metering_1=as.numeric(dat$Sub_metering_1)
+dat$Sub_metering_2=as.numeric(dat$Sub_metering_2)
+dat$Sub_metering_3=as.numeric(dat$Sub_metering_3)
+plot(dat$Sub_metering_1,type="l",col="black",xlab="",ylab="Energy sub metering")
+lines(dat$Sub_metering_2,type="l",col="red")
+lines(dat$Sub_metering_3,type="l",col="blue")
+legend("topright", legend = names(dat)[7:9], col=c("black","blue","red"), pch=16)
+dev.copy(png,"plot3.png")
+dev.off()
